a6528f0df0eac92fae67f73927996121b870c38d
Francisco Rangel <francorangelcoronado@gmail.com>
2024-04-05 00:42:12 -0600

    terminando la seccion de contacto

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,4 @@
+import ImagePortafolio from '../assets/undraw_portfolio_website_re_jsdd.svg'
 import './styles/contact.css'
 
 export const Contact = () => {
@@ -5,13 +6,19 @@ export const Contact = () => {
     <div className='contact-page'>
       <h1>Contact <strong>Me</strong></h1>
       <div className='contact-content'>
-        <div className='img-contact'>Imagen</div>
+        <div className='img-contact'>
+          <img src={ImagePortafolio} alt="img" />
+        </div>
         <div className='info-contact'>
           <div className='card-contact'>
             <h3>Email: francorangelcoronado@gmail.com</h3>
-            <h3>Github: @brunocoronado49</h3>
-            <h3>LinkedIn: Francisco Rangel</h3>
-            <h3>Download CV</h3>
+            <h3>
+              Github: <a href="https://github.com/brunocoronado49">@brunocoronado49</a>
+              </h3>
+            <h3>
+              LinkedIn: <a href="https://www.linkedin.com/in/jfrc117/">Francisco Rangel</a>
+            </h3>
+            <button className='btn-contact'>Download CV</button>
           </div>
         </div>
       </div>
